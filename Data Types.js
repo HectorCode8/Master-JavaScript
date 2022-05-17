790fabf974c2dea4c87126f5cce0c45e2ed05efa
Hector Haro H <hector8.haro@gmail.com>
2022-05-16 23:24:04 -0500

    Assingment

--- Data Types.js
+++ Data Types.js
@@ -17,3 +17,27 @@ let children
 // SYMBOL (ES2015) Value that is unique and cannot be changed [Not useful for now]
 
 // BIGINIT(ES2020) Larger integers than the Number type can hold
+
+/*
+LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+
+
+let isIsland = false
+let language;
+let all = isIsland + languages
+
+console.log(all)
+
+
+
+
+
+
+
+
