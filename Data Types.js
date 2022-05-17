7284d6089ea11afa53d217e24a413dedea71545c
Hector Haro H <hector8.haro@gmail.com>
2022-05-16 23:08:03 -0500

    Data Types

---
+++ Data Types.js
@@ -0,0 +1,10 @@
+// 7 Primitive DATA TYPES 
+//NUMBER Floating point numbers used for decimals and integers
+let age = 23
+
+// STRING Sequence of characters used for text
+let firstName = "Hector"
+
+// BOOLEAN Logical type that can only be true or false used for taking decisions
+let fullAge = true
+
