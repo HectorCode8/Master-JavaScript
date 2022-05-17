4350e552565c96377f5e11bda1d8fdd9c9db1fe0
Hector Haro H <hector8.haro@gmail.com>
2022-05-16 23:17:13 -0500

    All data types

--- Data Types.js
+++ Data Types.js
@@ -1,4 +1,5 @@
 // 7 Primitive DATA TYPES 
+
 //NUMBER Floating point numbers used for decimals and integers
 let age = 23
 
@@ -8,3 +9,11 @@ let firstName = "Hector"
 // BOOLEAN Logical type that can only be true or false used for taking decisions
 let fullAge = true
 
+// UNDEFINED Value taken by a variable that is not yet defined("empty value")
+let children
+
+// NULL Also means ' empty value'
+
+// SYMBOL (ES2015) Value that is unique and cannot be changed [Not useful for now]
+
+// BIGINIT(ES2020) Larger integers than the Number type can hold
