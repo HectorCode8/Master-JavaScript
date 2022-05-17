59760c9bdfb75181879c4a52a5fb36e22b1c3f0d
Hector Haro H <hector8.haro@gmail.com>
2022-05-16 21:57:25 -0500

    Excersize 1

--- Values and Variables.js
+++ Values and Variables.js
@@ -8,3 +8,12 @@ let years = 29
 console.log(firstName + " tiene " + years + " años" )
 let pi = Math.PI
 console.log(pi)
+
+// LECTURE: Values and Variables
+// Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+
+let country = " Mexico "
+let continent = " America "
+let population = 80000000
+console.log(`${continent} y su pais ${country} tienen ${population} millones de personas`)
