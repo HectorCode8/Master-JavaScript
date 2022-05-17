167c1996acc95a72bacc842b6db491c73ec6aa1e
Hector Haro H <hector8.haro@gmail.com>
2022-05-16 23:32:21 -0500

    let, const and var

---
+++ let, const and var.js
@@ -0,0 +1,18 @@
+// LET its mutable
+
+let age = 29
+age = 33
+console.log(age)
+
+// VAR old way define variable (not recomended)
+
+var job = "programmer"
+job = "teacher"
+
+
+
+// CONST is inmutable
+
+const newAge = 20
+newAge = 21
+console.log(newAge)
