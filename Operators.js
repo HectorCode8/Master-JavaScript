513051a19e4970e2499f8c669b67f9f770bc6be1
Hector Haro H <hector8.haro@gmail.com>
2022-05-18 00:09:55 -0500

    Operators

---
+++ Operators.js
@@ -0,0 +1,15 @@
+// Rest
+const ageHector = 2037 - 1993
+console.log(ageHector)
+
+// Sum
+const ageChocha = 1992 + 30
+console.log(ageChocha)
+
+// Mult
+const hector = 3
+const mult = hector * 2
+console.log(mult)
+
+// Div
+console.log(hector / 3)
