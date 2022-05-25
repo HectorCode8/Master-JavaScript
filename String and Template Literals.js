a96f0bf7c0fe9aec6f3e56b569a5f6a5bc7c5cce
Hector Haro H <hector8.haro@gmail.com>
2022-05-24 22:11:04 -0500

    add templeate literals

---
+++ String and Template Literals.js
@@ -0,0 +1,8 @@
+const firstName = 'hector'
+const job = 'Developer'
+const birthYear = 1993
+const year = 2037
+
+const hector = "I'm " + firstName + ', a ' + ( year - birthYear ) + ' years old ' + job + '!!'
+
+console.log(hector)
