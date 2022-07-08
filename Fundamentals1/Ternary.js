630496dbe16b2d61ab14f31b03bfab7c870723fc
Hector Haro H <hector8.haro@gmail.com>
2022-07-07 21:38:29 -0500

    add folders

---
+++ Fundamentals1/Ternary.js
@@ -0,0 +1,7 @@
+const age = 17
+age >= 18 ? console.log('You can drink wine 🍷') : console.log('You can not drink wine 🍷')
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧'
+console.log(drink)
+
+console.log(`You can drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`)
