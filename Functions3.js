f1045d74f393cd000044b0180dd43d7326059378
Hector Haro H <hector8.haro@gmail.com>
2022-07-08 17:42:40 -0500

    fucntions calling other functions

---
+++ Functions3.js
@@ -0,0 +1,16 @@
+// Functions calling other functions
+
+function cutFruit(fruit) {
+    return fruit * 4
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruit(apples)
+    const orangePieces = cutFruit(oranges)
+
+    const juice = `Juice with ${applePieces} pices of apples and ${orangePieces} pices of oranges`;
+    return juice;
+}
+
+fruitProcessor(2, 3);
+console.log(fruitProcessor(2, 3));
--- Fundamentals2/Functions2.js
+++ Fundamentals2/Functions2.js
@@ -19,3 +19,12 @@ const calcAge3 = (birthYear) => {
     return 2022 - birthYear;
 }
 console.log(calcAge3(2002));
+
+const yearUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearUntilRetirement(1993, 'Hector'));
+console.log(yearUntilRetirement(2002, 'Alondra'));
