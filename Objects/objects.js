4172976aa218ad5e2eeefc11c99c8ba2e0e0537f
Hector Haro H <hector8.haro@gmail.com>
2022-07-11 15:40:22 -0500

    Objects

---
+++ Objects/objects.js
@@ -0,0 +1,16 @@
+const hectorArray = [
+    'Hector',
+    'haro',
+    1993 - 2022,
+    'developer',
+    ['html', 'css', 'javascript', 'react']
+]
+
+const hector = {
+    name: 'Hector',
+    age: 22,
+    job: 'developer',
+    hobbies: ['html', 'css', 'javascript', 'react']
+}
+
+console.log(hector.hobbies)
