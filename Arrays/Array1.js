ca16f645c905f020aca13171cec039eff802bc65
Hector Haro H <hector8.haro@gmail.com>
2022-07-11 12:32:18 -0500

    arrays

---
+++ Arrays/Array1.js
@@ -0,0 +1,14 @@
+
+const array1 = ["Alo", "Agu", "Car", "Ped", "Mim"];
+const array2 = ["f", "g", "h", "i", "j"];
+
+console.log(array1[1]);
+console.log(array1.length);
+console.log(array1[ array1.length - 1 ]);
+
+array1[1] = "Agua";
+console.log(array1);
+
+const firstName = 'hector'
+const hector = [firstName, 'haro', 1993]
+console.log(hector);
