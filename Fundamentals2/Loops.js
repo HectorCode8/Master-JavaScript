f2e3ca576b63fc1973cadbad02fa500b9362eea9
Hector Haro H <hector8.haro@gmail.com>
2022-07-14 15:03:44 -0500

    loops 2

--- Fundamentals2/Loops.js
+++ Fundamentals2/Loops.js
@@ -1,5 +1,14 @@
 
+const hector = [
+    "Hector1",
+    "Hector2",
+    "Hector3",
+    "Hector4",
+    "Hector5",
+    "Hector6",
+]
 
-for(let rep = 1; rep <= 10; rep++){
-    console.log("Levante psesas repetecion " + rep + " 🏋️🏋️🏋️")
+for(let i = 0; i < hector.length ; i++) {
+    console.log(hectorArray[i])
 }
+
